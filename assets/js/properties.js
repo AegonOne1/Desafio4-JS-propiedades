a74d7235d484fed5c4f0b6751361f7b8145d354e
Daniel Ferrada <daniel.ferradabarria@gmail.com>
2024-01-10 21:00:08 -0300

    adding documents

---
+++ assets/js/properties.js
@@ -0,0 +1,67 @@
+const propiedadesAlquiler = [
+    {
+        nombre: '',
+        src: '',
+        descripcion: '',
+        ubicacion: '',
+        habitaciones: '',
+        costo: '',
+        smoke: '',
+        pets: '',
+    },
+    {
+        nombre: '',
+        src: '',
+        descripcion: '',
+        ubicacion: '',
+        habitaciones: '',
+        costo: '',
+        smoke: '',
+        pets: '',
+    },
+    {
+        nombre: '',
+        src: '',
+        descripcion: '',
+        ubicacion: '',
+        habitaciones: '',
+        costo: '',
+        smoke: '',
+        pets: '',
+    },
+    
+];
+
+const propiedadesVentas = [
+    {
+        nombre: '',
+        src: '',
+        descripcion: '',
+        ubicacion: '',
+        habitaciones: '',
+        costo: '',
+        smoke: '',
+        pets: '',
+    },
+    {
+        nombre: '',
+        src: '',
+        descripcion: '',
+        ubicacion: '',
+        habitaciones: '',
+        costo: '',
+        smoke: '',
+        pets: '',
+    },
+    {
+        nombre: '',
+        src: '',
+        descripcion: '',
+        ubicacion: '',
+        habitaciones: '',
+        costo: '',
+        smoke: '',
+        pets: '',
+    },
+]
+
